17242600e926ed8be08f67c1a7969c6b88a6abe7
Ričrd <155398193+Risanekk@users.noreply.github.com>
2026-02-09 10:48:07 +0100

    fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+shared_scripts {
+  '@ox_lib/init.lua',
+  '@lation_ui/init.lua',
+  'config.lua',
+}
+
+server_scripts {
+  '@oxmysql/lib/MySQL.lua',
+  'server.lua',
+}
+
+client_scripts {
+  'client.lua',
+}
+
+dependency 'ox_lib'
+dependency 'ox_inventory'
